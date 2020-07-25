bdde913b68b1a67f4c5a9de1ab7351dbffce0d98
Allan Sendagi <sendagiallan87@gmail.com>
2020-07-25 17:46:14 +0400

    Update index.js

--- index.js
+++ index.js
@@ -68,7 +68,7 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 app.use('/', router)
-app.listen("http://localhost:3000/landing_page")
+app.listen("https://allansendagi.github.io/landing_page/")
 
 
 
